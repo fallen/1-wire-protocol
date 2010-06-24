4517416222066476cba46421cc6a808420e7b86b
Yann Sionneau <yann@minet.net>
2010-06-24 03:08:55 +0200

    fifo read and write operations

--- hardware/fifo.v
+++ hardware/fifo.v
@@ -1,35 +1,62 @@
-module fifo(input clock, input reset, input do_write, input di, output do, input valid_input, input ack_output);
+module fifo(input clock, input reset, input do_read, input do_write, input [7:0] di, output [7:0] do, output write_ack, output read_ack,  output busy);
 
-reg read_pointer = 0;
-reg write_pointer = 0;
+reg [10:0] read_pointer = 11'd0;
+reg [10:0] old_read_pointer = 11'd0;
+reg [10:0] write_pointer = 11'd0;
+reg [10:0] old_write_pointer = 11'd0;
+reg [10:0] addr = 0;
 reg we = 0;
 wire has_remaining_bytes;
+reg pointer_overflow = 0;
+reg busy = 0;
+reg write_ack;
+reg read_ack;
+wire can_read;
 
-assign has_remaining_bytes = (
+assign can_read = (~pointer_overflow && (read_pointer < write_pointer)) || (pointer_overflow && (read_pointer >= write_pointer));
+assign can_write = (~pointer_overflow && (write_pointer >= read_pointer)) || (pointer_overflow && (read_pointer > write_pointer));
 
 bram bram1(clock, we, addr, di, do);
 
+
 always @(posedge clock)
 begin
-	if (valid_input & ~busy)
+	if ( (old_read_pointer > read_pointer) || (old_write_pointer > write_pointer) )
 	begin
-		if (do_write)
+		pointer_overflow <= ~pointer_overflow;
+	end
+end
+
+always @(posedge clock)
+begin
+	if ( (do_write || do_read) & ~busy)
+	begin
+		if (do_write & can_write)
 		begin
+			write_ack <= 1'b1;
 			addr <= write_pointer;
+			old_write_pointer <= write_pointer;
 			write_pointer <= write_pointer + 1;
 			we <= 1'b1;
 			busy <= 1'b1;
 		end
 		else
 		begin
-			we <= 1'b0;
-			addr <= read_pointer;
-			read_pointer <= read_pointer + 1;
-			busy <= 1'b1;
+			if (do_read & can_read)
+			begin
+				we <= 1'b0;
+				read_ack <= 1'b1;
+				addr <= read_pointer;
+				old_read_pointer <= read_pointer;
+				read_pointer <= read_pointer + 1;
+				busy <= 1'b1;
+			end
 		end
 	end
 	else
 	begin
+		read_ack <= 1'b0;
+		write_ack <= 1'b0;
 		busy <= 1'b0;
 		we <= 1'b0;
 	end
